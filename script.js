e13461ac1a11c171c0733d4cdbf6bfbf886f5c4d
MutyaOP <mutyaytlp@gmail.com>
2025-03-06 15:22:41 +0500

    patch 2

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function openEnvelope() {
     const envelope = document.querySelector('.envelope');
     envelope.classList.toggle('open');
 }
+function toggleMusic() {
+    let music = document.getElementById("bgMusic");
+    if (music.paused) {
+        music.play();
+    } else {
+        music.pause();
+    }
+}
